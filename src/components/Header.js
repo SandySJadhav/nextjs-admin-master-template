e1205093992d38d22083814842a023562c06d437
Sandip Jadhav <sandip.jadhav@kohler.com>
2022-12-04 16:34:10 +0530

    updated search box

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,3 @@
-import classNames from "classnames";
 import React, { useState } from "react";
 import {
   HiSun,
@@ -6,6 +5,7 @@ import {
   HiUser,
   HiOutlineCog,
   HiOutlineMenu,
+  HiSearch,
 } from "react-icons/hi";
 import IconButton from "./IconButton";
 
@@ -24,28 +24,28 @@ const Header = ({ panel, toggleSidePanel }) => {
 
   return (
     <header className="dark:bg-base p-2 bg-white shadow-md">
-      <nav
-        className={classNames(
-          "flex flex-row",
-          {
-            "justify-between": !panel,
-          },
-          {
-            "justify-end": panel,
-          }
-        )}
-      >
-        <div
-          className={classNames(
-            { hidden: panel },
-            {
-              flex: !panel,
-            }
-          )}
-        >
-          <IconButton onClick={toggleSidePanel}>
-            <HiOutlineMenu />
-          </IconButton>
+      <nav className="flex flex-row justify-between">
+        <div className="flex">
+          {!panel ? (
+            <IconButton onClick={toggleSidePanel}>
+              <HiOutlineMenu />
+            </IconButton>
+          ) : null}
+
+          <div className="grid content-center pl-4">
+            <label className="relative flex h-10">
+              <span className="sr-only">Search</span>
+              <span className="absolute inset-y-0 left-0 flex items-center pl-2 text-slate-400">
+                <HiSearch />
+              </span>
+              <input
+                className="placeholder:italic placeholder:text-slate-400 block bg-white dark:bg-black w-full border border-slate-300 rounded-md py-2 pl-9 pr-3 shadow-sm focus:outline-none focus:border-sky-500 focus:ring-sky-500 focus:ring-1 sm:text-sm"
+                placeholder="Search..."
+                type="text"
+                name="search"
+              />
+            </label>
+          </div>
         </div>
         <div className="flex">
           <IconButton onClick={toggleTheme}>
