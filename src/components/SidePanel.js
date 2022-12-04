d94ebfcc8c4d44ce53dbaebe9239cfbacd4db8b7
Sandip Jadhav <sandip.jadhav@kohler.com>
2022-12-04 16:54:18 +0530

    profile image added

--- src/components/SidePanel.js
+++ src/components/SidePanel.js
@@ -1,3 +1,4 @@
+import Image from "next/image";
 import React from "react";
 import { HiOutlineMenu } from "react-icons/hi";
 import IconButton from "./IconButton";
@@ -9,6 +10,16 @@ const SidePanel = ({ panel, toggleSidePanel }) => {
         <IconButton onClick={toggleSidePanel} className="bg-sidebar">
           <HiOutlineMenu />
         </IconButton>
+      </div>
+      <div className="grid justify-items-center">
+        <Image
+          className="rounded-full border-8 border-base dark:border-black"
+          height={100}
+          width={100}
+          src="/1605610886733.jpg"
+          alt="profile"
+        />
+        <div>Sandip Jadhav</div>
       </div>
     </nav>
   );
