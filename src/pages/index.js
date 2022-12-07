52462f1413c4d05abd03d20e252a2eb7da51fb98
Sandip Jadhav <sandip.jadhav@kohler.com>
2022-12-07 22:46:14 +0530

    added home page title

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,11 +4,11 @@ import React from "react";
 const index = () => {
   return (
     <Layout>
-      <div>
-        <h1>Welcome to NextJS Admin Template Master</h1>
-      </div>
-      <div>
-        <p>I have uploaded a few samples</p>
+      <div className="h-screen grid grid-cols-1 mt-10">
+        <div className="justify-self-center">
+          <h1 className="font-bold text-4xl">Welcome to NextJS Admin Template Master</h1>
+          <p className="mt-4">Don't forget to checkout samples</p>
+        </div>
       </div>
     </Layout>
   );
