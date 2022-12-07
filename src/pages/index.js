b9d8a1da53ec3d73f1632531f0d99137933039f6
Sandip Jadhav <sandip.jadhav@kohler.com>
2022-12-07 22:47:07 +0530

    apos

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ const index = () => {
       <div className="h-screen grid grid-cols-1 mt-10">
         <div className="justify-self-center">
           <h1 className="font-bold text-4xl">Welcome to NextJS Admin Template Master</h1>
-          <p className="mt-4">Don't forget to checkout samples</p>
+          <p className="mt-4">Don&apos;t forget to checkout samples</p>
         </div>
       </div>
     </Layout>
